0ce15d49a90f41b802fe39c6a4ac406da3dfab18
Shakeel Gavioli <shakeelgavioli@localhost.localdomain>
2019-01-22 19:13:57 +0000

    all tilting functions

---
+++ intersection tilt single.R
@@ -0,0 +1,49 @@
+intersection_tilt_single <- function(v1, v2, X, r) {
+  
+  #  transform data to have mean 0 and norm 1 
+  X <- as.matrix(X)
+  for (i in 1:ncol(X)) {
+    X[,i] <- X[,i] - mean(X[,i])
+    X[,i] <- X[,i]/sqrt(sum(X[,i]^{2}))
+  }
+  
+  #  extract vectors and conditioning matrix
+  x1 <- X[,v1]
+  x2 <- X[,v2]
+  X <- X[,-c(v1,v2)]
+  
+  # define empty conditioning set
+  # find highly associated variables
+  Z <- matrix(rep(1,nrow(X)), ncol = 1)
+  for (i in 1:ncol(X)) {
+    if (abs(t(X[,i]) %*% x1) > r 
+        & abs(t(X[,i]) %*% x2) > r){
+      Z <- cbind(Z, X[,i])
+    }
+  }
+  
+  #  if highly associated varaibles exist 
+  #  find tilted correlation 
+  if (ncol(Z) > 1){
+    
+    Z <- Z[,-1]
+    P <- Z %*% solve(t(Z) %*% Z) %*% t(Z)
+    M <- diag(1,nrow(X)) - P
+    
+    #  transfrom x1 
+    x1_str <- M %*% x1
+    a <- t(x1) %*% M %*% x1 
+    
+    #  transfrom x2 
+    x2_str <- M %*% x2
+    b <- t(x2) %*% M %*% x2
+    
+    tilt <- (sqrt(a*b))^{-1} * (t(x1_str) %*% x2_str)
+    
+    # else return sraight correlation
+  } else {
+    tilt <- t(x1) %*% x2
+  }
+  
+  return(tilt)
+}
---
+++ intersection tilt.R
@@ -0,0 +1,64 @@
+intersection_tilt <- function(X,r) {
+  
+  #  transform data to have mean 0 and norm 1 
+  X <- as.matrix(X)
+  for (i in 1:ncol(X)) {
+    X[,i] <- X[,i] - mean(X[,i])
+    X[,i] <- X[,i]/sqrt(sum(X[,i]^{2}))
+  }
+  
+  #  produce tilted correlation matrix 
+  tilt <- diag(1, ncol(X))
+  
+  for (i in 1:ncol(X)){
+    for (j in 1:ncol(X)){
+      if (i != j){
+        
+        #  define new (x,y) variables 
+        x <- X[,i]
+        y <- X[,j]
+        Xk <- X[,-c(i,j)]
+        Z <- matrix(rep(1, nrow(X)), ncol = 1)
+        
+        #  find highly associated variables 
+        for (k in 1:ncol(Xk)) {
+          if ( abs(t(x) %*% Xk[,k]) > r 
+               & abs(t(y) %*% Xk[,k]) > r) {
+            Z <- cbind(Z, Xk[,k])
+          }
+        }
+        
+        if (ncol(Z)>1) {
+          
+          #  if highly associated varaibles exist 
+          #  define projection matrix 
+          if (det(solve(t(Z) %*% Z)) == 0) stop("choose a higher threshold (r), 
+                                                the projection matrix is singular!")
+          Z <- Z[,-1]
+          P <- Z %*% solve(t(Z) %*% Z) %*% t(Z)
+          M <- diag(1,nrow(X)) - P
+          
+          #  transfrom x 
+          x_str <- M %*% x
+          a <- t(x) %*% M %*% x 
+          
+          #  transfrom y 
+          y_str <- M %*% y
+          b <- t(y) %*% M %*% y 
+          
+          #  replace off diagonals 
+          tilt[i,j] = (sqrt(a*b))^{-1} * (t(x_str) %*% y_str)
+          
+        } else {
+          
+          #  if no highly associated vvaraibles exist 
+          #  use straight correlation 
+          tilt[i,j] = t(x) %*% y
+          
+        }
+      }
+    }
+  }
+  #  return tilted correlation
+  return(tilt)
+}
