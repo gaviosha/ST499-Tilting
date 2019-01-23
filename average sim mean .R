a1f7313dc71084ac65fb70423155faf8525bced0
Shakeel Gavioli <shakeelgavioli@localhost.localdomain>
2019-01-23 09:46:06 +0000

    addds functions for mean and max correlation simulations

---
+++ average sim mean .R
@@ -0,0 +1,40 @@
+# simulates average tilted correlation
+# using method 3.3
+# requires package ppcor 
+
+require(ppcor)
+
+average_sim_mean <- function(sims, n, p, Rmin, Rmax) {
+  
+  for (i in 1:sims) {
+    
+    cat("Beginning average simulation:", i)
+    cat("\n")
+    
+    #  simulate matrices 
+    W <- rnorm(n)
+    for (j in 1:(p-1)) {
+      W <- cbind(W, rnorm(n))
+    }
+    
+    #  threshold and holder 
+    R <- seq(from = Rmin, to = Rmax, by = 0.01)
+    xx <- c()
+    
+    for (k in 1:length(R)) {
+      xx <- c(xx, mean(abs(average_tilt(W, R[k]) - diag(1,p))))
+      cat(round(k/length(k), digits = 2), "% complete")
+      cat("\n")
+    }
+    
+    #  plot results 
+    plot(R,xx, type = "l", 
+         xlab = "Threshold", 
+         ylab = "maximum correlation")
+    lines(R, rep(mean(abs(cor(W) - diag(1,p))), length(R)), col = "red")
+    lines(R, rep(mean(abs(pcor(W)$estimate - diag(1,p))), length(R)), col = "green")    
+    
+    cat("\n")
+    cat("\n")
+  }
+}
---
+++ union sim max.R
@@ -0,0 +1,40 @@
+# simulates maximum tilted correlation
+# using method 3.2
+# requires package ppcor 
+
+require(ppcor)
+
+union_sim_max <- function(sims, n, p, Rmin, Rmax) {
+  
+  for (i in 1:sims) {
+    
+    cat("Beginning union simulation:", i)
+    cat("\n")
+    
+    #  simulate matrices 
+    W <- rnorm(n)
+    for (j in 1:(p-1)) {
+      W <- cbind(W, rnorm(n))
+    }
+    
+    #  threshold and holder 
+    R <- seq(from = Rmin, to = Rmax, by = 0.01)
+    xx <- c()
+    
+    for (k in 1:length(R)) {
+      xx <- c(xx, max(abs(union_tilt(W, R[k]) - diag(1,p))))
+      cat(round(k/length(k), digits = 2), "% complete")
+      cat("\n")
+    }
+    
+    #  plot results 
+    plot(R,xx, type = "l", 
+         xlab = "Threshold", 
+         ylab = "maximum correlation")
+    lines(R, rep(mean(abs(cor(W) - diag(1,p))), length(R)), col = "red")
+    lines(R, rep(mean(abs(pcor(W)$estimate - diag(1,p))), length(R)), col = "green")
+    
+    cat("\n")
+    cat("\n")
+  }
+}
